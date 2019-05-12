c8c789795f9b7062dd6c8963e590a19ea8e50056
AnakimL <lastdayoflife2@gmail.com>
2019-05-13 02:19:13 +0300

    Updates

---
+++ precache-manifest.c766f9776cfffeb8731b0776817f616a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bd85095567b8064ff74e",
+    "url": "/todo-react/static/css/main.6de77085.chunk.css"
+  },
+  {
+    "revision": "bd85095567b8064ff74e",
+    "url": "/todo-react/static/js/main.bd850955.chunk.js"
+  },
+  {
+    "revision": "58fa791636f4babc186f",
+    "url": "/todo-react/static/js/1.58fa7916.chunk.js"
+  },
+  {
+    "revision": "4ebad8e7a9f5566e1598",
+    "url": "/todo-react/static/js/runtime~main.4ebad8e7.js"
+  },
+  {
+    "revision": "6a47ad1c71d4ecbfaedb915954578c5c",
+    "url": "/todo-react/index.html"
+  }
+];
